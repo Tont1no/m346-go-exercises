54f0d0626f495042ec9d10f9d3dd6681ccee775f
Pascal Fratamico <Pascal.Fratamico@kauz.ch>
2024-11-21 16:24:29 +0100

    Ex-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
+
+	var firstName string = "Pasacl"
+	var lastName string = "Fratamico"
+
+	var dayOfBirth int = 18
+	var monthOfBirth int = 1
+	var yearOfBirth int = 2005
+	var numberOfSiblings int = 3
+	var heightInMeters float32 = 1.8
+	var zodiacSign = '\u2651'
+
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
