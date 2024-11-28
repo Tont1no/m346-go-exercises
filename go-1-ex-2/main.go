3b2c19257df82e292373c3385260749845e7cf34
Pascal Fratamico <Pascal.Fratamico@kauz.ch>
2024-11-28 15:53:57 +0100

    Update: 1-ex-2, Formel

--- go-1-ex-2/main.go
+++ go-1-ex-2/main.go
@@ -17,6 +17,6 @@ func main() {
 	fmt.Printf("a marathon is %.2f kilometres = %.2f miles long\n", marathonInKM, marathonInMiles)
 
 	var boilingWaterCelsius = 100.0
-	var boilingWaterFahrenheit = boilingWaterCelsius*(9/5) + 32 // TODO: calculate using formula above!
+	var boilingWaterFahrenheit = boilingWaterCelsius*9/5 + 32 // TODO: calculate using formula above!
 	fmt.Printf("water boils at %.2f°C = %.2f°F\n", boilingWaterCelsius, boilingWaterFahrenheit)
 }
