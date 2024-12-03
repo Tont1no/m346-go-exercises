aff80aa6a7033abc45457840f9fdda5a40143914
Pascal Fratamico <Pascal.Fratamico@kauz.ch>
2024-12-03 11:51:16 +0100

    Update: 2-ex-1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	FullName
+	BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName: FullName{
+			FirstName: "Pascal",
+			LastName:  "Fratamico",
+		},
+		BirthDate: BirthDate{
+			Day:   18,
+			Month: 1,
+			Year:  2005,
+		},
+		NumberOfSiblings: 2,        // TODO: adjust
+		ZodiacSign:       '\u2651', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
