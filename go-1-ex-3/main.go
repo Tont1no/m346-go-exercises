4b7f454fc2cee412ef0003cbc5ca4d1c9826e6c5
Pascal Fratamico <Pascal.Fratamico@kauz.ch>
2024-12-03 11:28:34 +0100

    Update: ex-3

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -11,11 +12,30 @@ func main() {
 	var when = time.Now()
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	fmt.Fprintln(os.Stdout, "the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
 	// go run ex3/main.go TODO
+
+	eyesFile, err := os.Create("eyes.txt")
+	if err != nil {
+		fmt.Println("Error creating eyes.txt:", err)
+		return
+	}
+	defer eyesFile.Close()
+
+	logFile, err := os.Create("dice.log")
+	if err != nil {
+		fmt.Println("Error creating dice.log:", err)
+		return
+	}
+	defer logFile.Close()
+
+	fmt.Fprintln(eyesFile, "the dice shows", eyes, "eyes")
+	fmt.Fprintln(logFile, "the dice shows", eyes, "eyes")
+	fmt.Fprintln(logFile, "the dice was rolled at", when)
+
 }
